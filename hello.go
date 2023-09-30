db6988c9dfc0c278bc53ed5440b8eec2cee74fe8
Drahlous <MattTStachowiak@gmail.com>
2023-09-29 21:56:50 -0600

    Read line-by-line from stdin

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+    "bufio"
+)
 
 func main() {
     fmt.Println("Hello world.")
+    
+    // Read line-by-line
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        line := scanner.Text()
+        fmt.Println("read line: ", line)
+    }
 }
