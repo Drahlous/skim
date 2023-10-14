438b807191e99534f5b6ff5d6844e6e8f30dd99e
Drahlous <MattTStachowiak@gmail.com>
2023-10-14 14:52:20 -0600

    Move filter view to a separate file

--- ui/ui.go
+++ ui/ui.go
@@ -3,12 +3,12 @@ package ui
 import (
 	"bufio"
 	"example/user/skim/filterfiles"
+	filterview "example/user/skim/ui/views/filterview"
 	logview "example/user/skim/ui/views/logview"
 	"fmt"
 	"os"
 
 	// We'll shorten the package name to "tea" for ease of use
-	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -29,39 +29,10 @@ type TableView interface {
 	GetMaxCursor() int
 }
 
-type FilterView struct {
-	cursor  int // which filter our cursor is pointing at
-	table   table.Model
-	filters []filterfiles.Filter
-}
-
-func (v *FilterView) Toggle() {
-	filter := &v.filters[v.cursor]
-	filter.IsEnabled = !filter.IsEnabled
-}
-
-func (v *FilterView) CursorUp() int {
-	if v.cursor > 0 {
-		v.cursor--
-	}
-	return v.cursor
-}
-
-func (v *FilterView) CursorDown() int {
-	if v.cursor < v.GetMaxCursor() {
-		v.cursor++
-	}
-	return v.cursor
-}
-
-func (v *FilterView) GetMaxCursor() int {
-	return len(v.filters) - 1
-}
-
 // Model to store the application's state
 type model struct {
 	log           logview.LogView
-	filters       FilterView
+	filters       filterview.FilterView
 	focus         Focus // which view is currently in focus
 	windowWidth   int
 	windowHeight  int
@@ -72,12 +43,6 @@ var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
-var filterStyle = lipgloss.NewStyle().
-	Bold(false).
-	Foreground(lipgloss.Color("#000000")).
-	PaddingTop(0).
-	PaddingLeft(0)
-
 // Define the initial state for the application
 func initialModel(filters []filterfiles.Filter, scanner *bufio.Scanner) model {
 	var lines []string
@@ -85,9 +50,9 @@ func initialModel(filters []filterfiles.Filter, scanner *bufio.Scanner) model {
 		lines = append(lines, scanner.Text())
 	}
 	return model{
-		filters: FilterView{
-			filters: filters,
-			cursor:  0,
+		filters: filterview.FilterView{
+			Filters: filters,
+			Cursor:  0,
 		},
 		log: logview.LogView{
 			Lines:  lines,
@@ -161,42 +126,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func makeFilters(m model) table.Model {
-	columns := []table.Column{
-		{Title: "", Width: 3},
-		{Title: "Regex", Width: m.windowWidth - 9}, // TODO: Avoid hardcoding this offset
-	}
-
-	rows := []table.Row{}
-
-	// Iterate over filters
-	for i, filter := range m.filters.filters {
-
-		// Is this filter enabled?
-		checked := " " // not selected
-		if m.filters.filters[i].IsEnabled {
-			checked = "x" // this item is selected
-		}
-
-		style := filterStyle
-		style.Background(lipgloss.Color(filter.BackColor))
-
-		// Render the row
-		row := table.Row{fmt.Sprintf("[%s]", checked), style.Render(filter.XML.Text)}
-		rows = append(rows, row)
-	}
-
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(5),
-	)
-
-	t.MoveDown(m.filters.cursor)
-	return t
-}
-
 // The View method will simply look at the model and return a string.
 // The returned string is our UI.
 // Bubble Tea takes care of redrawing and other logic.
@@ -205,11 +134,11 @@ func (m model) View() string {
 	s := ""
 
 	// Make table of filtered log lines
-	m.log.MakeTable(m.windowWidth, m.windowHeight, m.filters.filters, m.hideUnmatched)
+	m.log.MakeTable(m.windowWidth, m.windowHeight, m.filters.Filters, m.hideUnmatched)
 	s += baseStyle.Render(m.log.Table.View()) + "\n"
 
-	m.filters.table = makeFilters(m)
-	s += baseStyle.Render(m.filters.table.View()) + "\n"
+	m.filters.MakeTable(m.windowWidth, m.windowHeight, m.filters.Filters)
+	s += baseStyle.Render(m.filters.Table.View()) + "\n"
 
 	// Send the UI for rendering
 	return s
---
+++ ui/views/filterview/filterview.go
@@ -0,0 +1,82 @@
+package filterview
+
+import (
+	"example/user/skim/filterfiles"
+	"fmt"
+	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/lipgloss"
+	//"strings"
+)
+
+type FilterView struct {
+	Cursor  int // which filter our cursor is pointing at
+	Table   table.Model
+	Filters []filterfiles.Filter
+}
+
+func (v *FilterView) Toggle() {
+	filter := &v.Filters[v.Cursor]
+	filter.IsEnabled = !filter.IsEnabled
+}
+
+func (v *FilterView) CursorUp() int {
+	if v.Cursor > 0 {
+		v.Cursor--
+	}
+	return v.Cursor
+}
+
+func (v *FilterView) CursorDown() int {
+	if v.Cursor < v.GetMaxCursor() {
+		v.Cursor++
+	}
+	return v.Cursor
+}
+
+func (v *FilterView) GetMaxCursor() int {
+	return len(v.Filters) - 1
+}
+
+var filterStyle = lipgloss.NewStyle().
+	Bold(false).
+	Foreground(lipgloss.Color("#000000")).
+	PaddingTop(0).
+	PaddingLeft(0)
+
+func (v *FilterView) MakeTable(windowWidth int, windowHeight int, filters []filterfiles.Filter) table.Model {
+	columns := []table.Column{
+		{Title: "", Width: 3},
+		{Title: "Regex", Width: windowWidth - 9}, // TODO: Avoid hardcoding this offset
+	}
+
+	rows := []table.Row{}
+
+	// Iterate over filters
+	for i, filter := range filters {
+
+		// Is this filter enabled?
+		checked := " " // not selected
+		if filters[i].IsEnabled {
+			checked = "x" // this item is selected
+		}
+
+		style := filterStyle
+		style.Background(lipgloss.Color(filter.BackColor))
+
+		// Render the row
+		row := table.Row{fmt.Sprintf("[%s]", checked), style.Render(filter.XML.Text)}
+		rows = append(rows, row)
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(5),
+	)
+
+	t.MoveDown(v.Cursor)
+
+	v.Table = t
+	return t
+}
